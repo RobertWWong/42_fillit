74e3a6685ba61aabe710f34de3cb805f60c3ab8c
robertwwong <robertww@uci.edu>
2018-07-20 10:48:23 -0700

    Adding libft to our fillit for strsplit usages later.

---
+++ ft_fillit.h
@@ -0,0 +1,15 @@
+#ifndef FT_FILLIT_H
+# define FT_FILLIT_H
+
+# include <string.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+typedef struct		s_terminos
+{
+	int				height;
+	int				width;
+	int				shape[2];
+}					t_terminos;
+
+#endif
