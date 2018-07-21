384edfcb090a8e658f64c6a711777d9b3248701a
Zexi Wang <zwang@e1z3r6p5.42.us.org>
2018-07-21 11:57:00 -0700

    now we got the files

--- includes/tetrimino.h
+++ includes/tetrimino.h
@@ -6,7 +6,7 @@
 /*   By: zwang <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/19 09:45:28 by zwang             #+#    #+#             */
-/*   Updated: 2018/07/20 15:50:15 by zwang            ###   ########.fr       */
+/*   Updated: 2018/07/21 11:53:37 by zwang            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,28 @@
 # include <stdlib.h>
 # include "libft.h"
 
+
 int				count_char(int file_desc);
 char			*get_data(char *file_name, char *data);
 int				ft_strcount(char const *s, char c);
 char			**get_tetriminoes(char *data, char **tetriminoes);
 int				basic_validate(char *data);
 
+typedef struct	s_vertex
+{
+	int			x;
+	int			y;
+}				t_vertex;
+
+typedef struct	s_tetrimino
+{
+	int			height;
+	int			width;
+	t_vertex	*upper;
+	t_vertex	*lower;
+	t_vertex	*left;
+	t_vertex	*right;
+	t_tetrimino	*next;
+}				t_tetrimino;
 
 #endif
