767b0b9ce074316a18390a5839eeb44be3802a94
Zexi Wang <zwang@e1z3r4p3.42.us.org>
2018-07-23 11:17:50 -0700

    update on fill_square function

--- srcs/ft_fillit.c
+++ srcs/ft_fillit.c
@@ -6,7 +6,7 @@
 /*   By: zwang <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/22 19:09:36 by zwang             #+#    #+#             */
-/*   Updated: 2018/07/23 10:14:24 by zwang            ###   ########.fr       */
+/*   Updated: 2018/07/23 11:17:17 by zwang            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,20 @@
 #include "../includes/file_ops.h"
 #include "../includes/tetro.h"
 
+int		count_tetroes(char **tetro_strings)
+{
+	int	i;
+
+	i = 0;
+	while (tetro_strings[i])
+		i++;
+	return (i);
+}
+
 t_tetro		**create_tetro_list(char **tetro_strings, int tetro_cnt)
 {
-	static t_tetro	**tetro_list;
-	int				i;
+	t_tetro		**tetro_list;
+	int			i;
 
 	i = -1;
 	if (!(tetro_list = malloc(sizeof(t_tetro*) * tetro_cnt + 1)))
@@ -27,53 +37,38 @@ t_tetro		**create_tetro_list(char **tetro_strings, int tetro_cnt)
 	return (tetro_list);
 }
 
-int		count_tetroes(char **tetro_strings)
+t_board		*create_board(t_tetro **tetro_list, int tetro_cnt)
 {
-	int	i;
-
-	i = 0;
-	while (tetroes[i])
-		i++;
-	return (i);
-}
-
-int			ft_sqrt(int	num)
-{
-	float	x;
-	float	y;
-	float	e;
-
-	x = n;
-	y = 1;
-	e = 0.000001;
-	while (x - y > e)
-	{
-		x = (x + y) / 2;
-		y = n / x;
-	}
-	return ((int)x);
-}
-
-t_board		*create_board(char **tetroes, int size, int size_increase)
-{
-	int		num;
+	int		i;
+	int		j;
 	t_board	*board;
 	char	**board_state;
 
 	board = (t_board *)malloc(sizeof(t_board));
-	board->tetro_amt = count_tetroes(tetroes);
-	if (size_increase)
-		board->sq_len = size + 1;
-	else
-		board->sq_len = ft_sqrt(board->tetro_amt * 4);
-	board_state = (char **)malloc(size(char *) * board->sq_len);
+	board->tetro_amt = tetro_cnt; 
+	board->sq_len = ft_sqrt(board->tetro_amt * 4);
+	board->tetro_list = tetro_list;
+	board_state = (char **)malloc(sizeof(char *) * board->sq_len);
+	i = -1;
+	while (++i < board->sq_len)
+	{
+		board_state[i] = ft_strnew(board->sq_len + 1);
+		j = -1;
+		while (++j < board->sq_len)
+			board_state[i][j] = '.';
+	}
 	board->board_state = board_state;
 	return (board);
 }
 
+t_board		*increment_board(t_board *board)
+{
+	char	**board_state;
+
+
 int			is_safe(t_board *board, t_tetro *tetro, t_point *pos)
 {
-	int		i;
+	int	i;
 	
 	i = 0;
 	while (i < 4 && (tetro->points)[i].x + pos->x < board->sq_len &&
@@ -108,9 +103,10 @@ int			backtrack_map(t_board *board, int tetro_index)
 	int		j;
 	t_point	*pos;
 	
-	if (board->tetro_amot == 0)
+	if (board->tetro_amt == 0)
 		return (1);
 	i = 0;
+	pos = (t_point *)malloc(sizeof(t_point));
 	while (i < board->sq_len)
 	{
 		j = 0;
@@ -118,12 +114,12 @@ int			backtrack_map(t_board *board, int tetro_index)
 		{
 			pos->x = i;
 			pos->y = j;
-			if (is_safe(board, tetroes[tetro_index], pos))
+			if (is_safe(board, (board->tetro_list)[tetro_index], pos))
 			{
-				remove_or_add(board, tetroes[tetro_index], pos, 1);
+				remove_or_add(board, (board->tetro_list)[tetro_index], pos, 1);
 				if (backtrack_map(board, tetro_index + 1))
 					return (1);
-				remove_or_add(board, tetroes[tetro_index], pos, 0);
+				remove_or_add(board, (board->tetro_list)[tetro_index], pos, 0);
 			}
 			j++;
 		}
@@ -132,6 +128,18 @@ int			backtrack_map(t_board *board, int tetro_index)
 	return (0);
 }
 
-int			fill_square(char **tetro_strings)
+char		**fill_square(char **tetro_strings)
 {
+	int		tetro_cnt;
+	int		tetro_index;
+	t_tetro	**tetro_list;
+	t_board	*board;
 
+	tetro_cnt = count_tetroes(tetro_strings);
+	tetro_list = create_tetro_list(tetro_strings, tetro_cnt);
+	board = create_board(tetro_list, tetro_cnt, 0, 0);
+	tetro_index = 0;
+	while (!backtrack_map(board, tetro_index))
+		board = create_board(tetro_list, tetro_cnt, board->sq_len, 1);
+	return (board->board_state);
+}
